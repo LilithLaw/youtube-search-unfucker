9718b11af0123728879dd921e20fcd0cc65abf1b
Parking Lot Studio <76890242+ParkingLotGames@users.noreply.github.com>
2023-01-28 13:18:52 -0600

    fixed issues: only activated after a reload, infinite loops when using filters, now listens on all youtube and intercepts search urls as they are performed instead of after search page load
    -Added "redirecting" and implemented check to prevent infinite loops.
    -Changed the event listener from "DOMContentLoaded" to "yt-navigate-finish"
    -Changed the condition for checking if the page is a search page from "test(location.search)" to "test(currentURL)"
    -Changed the method of redirecting the page from "window.location.href" to "window.location.replace()"

--- youtube-search-unfucker.user.js
+++ youtube-search-unfucker.user.js
@@ -1,12 +1,37 @@
 // ==UserScript==
 // @name        Youtube Search unfucker
-// @match       https://*.youtube.com/results*
-// @run-at      document-start
+// @match       https://*.youtube.com/*
+// @run-at      document-end
 // @grant       none
 // ==/UserScript==
+// We now listen globally on YouTube.com as we intercept search url's before they are fully loaded, hence we now listen on document-end
+
+// This variable is used to check if the script is currently redirecting the user to a new URL.
+// It is used to prevent occasional infinite loops when searching for videos.
+// More importantly it fixes an issue where modifying the filter settings results in an infinite loop every time.
+let redirecting = false;
+
+
+// Add an event listener that listens for the "yt-navigate-finish" event, which is fired when the page is fully loaded.
 document.addEventListener("yt-navigate-finish", function() {
-const fuckedURL = window.location.href
-if (((/[?&]sp=CAASAhAB/.test(location.search) === false)) && ((/search/.test(location.search) === true))) {
-  window.location.href = fuckedURL + "&sp=CAASAhAB"
-}
+    // Check if the script is currently in the process of redirecting the user to a new URL.
+    if (redirecting) {
+        // If the script is currently redirecting, return and do nothing.
+        return;
+    }
+
+    // Get the current URL of the page.
+    const fuckedURL = window.location.href;
+    
+    // Check if the current URL is a search page and if the "&sp=CAASAhAB" string is not already in the URL.
+    if (((/[?&]sp=CAASAhAB/.test(location.search) === false)) && ((/search/.test(fuckedURL) === true))) {
+        // Set the "redirecting" variable to "true" before performing the redirection.
+        redirecting = true;
+        // Create the new URL by appending the "&sp=CAASAhAB" string to the current URL.
+        const newURL = fuckedURL + "&sp=CAASAhAB";
+        // Don't use this, it may cause errors
+        // window.location.href = fuckedURL + "&sp=CAASAhAB"
+        // Instead we use the replace() method to redirect the user to the new URL, this will replace the current URL in the browser's history too.
+        window.location.replace(newURL);
+    }
 });
